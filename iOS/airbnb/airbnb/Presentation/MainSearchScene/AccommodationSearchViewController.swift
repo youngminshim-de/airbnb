7fdf0f34aa4f253d741453255182e1bc8e5caed6
shimyoungmin <dudals6965@gmail.com>
2021-08-16 14:14:37 +0900

    feat: [#5] Add AccommodationSearchViewController
    AccommodationSearchViewController 생성 및 UI 구현

---
+++ iOS/airbnb/airbnb/Presentation/MainSearchScene/AccommodationSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AccomodationSearchViewController.swift
+//  airbnb
+//
+//  Created by 심영민 on 2021/08/16.
+//
+
+import UIKit
+
+class AccommodationSearchViewController: UITableViewController {
+    
+}
+
+extension AccommodationSearchViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+}
