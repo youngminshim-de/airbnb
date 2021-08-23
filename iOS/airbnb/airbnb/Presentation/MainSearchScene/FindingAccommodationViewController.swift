e15ba7c8deb3e37cdd5b8d5d5141ab6ffcb2aa8d
shimyoungmin <dudals6965@gmail.com>
2021-08-23 20:28:57 +0900

    feat: [#8] Add FindingAccommodationViewController
    숙소찾기 뷰 컨트롤러 페이지 추가

---
+++ iOS/airbnb/airbnb/Presentation/MainSearchScene/FindingAccommodationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FindingAccommodationViewController.swift
+//  airbnb
+//
+//  Created by 심영민 on 2021/08/23.
+//
+
+import UIKit
+
+class FindingAccommodationViewController: UIViewController {
+
+    @IBOutlet weak var findingAccommodationConditionView: UIScrollView!
+    @IBOutlet weak var condtionStackView: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
