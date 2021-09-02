2d54263c6e1212a369e9632fdec39e8f616b78ea
shimyoungmin <dudals6965@gmail.com>
2021-09-02 16:31:38 +0900

    feat: [#12] Add AccommodationListCell
    숙소리스트 컬렉션 뷰에 들어가는 커스텀 셀 추가

---
+++ iOS/airbnb/airbnb/Presentation/MainSearchScene/AccommodationListCell.swift
@@ -0,0 +1,20 @@
+//
+//  AccommodationListCell.swift
+//  airbnb
+//
+//  Created by 심영민 on 2021/09/02.
+//
+
+import UIKit
+
+class AccommodationListCell: UICollectionViewCell {
+    @IBOutlet weak var headerFilterLabel: UILabel!
+    @IBOutlet weak var headerTitleLabel: UILabel!
+    @IBOutlet weak var accommodationImageView: UIImageView!
+    @IBOutlet weak var averageScoreLabel: UILabel!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var accommodationNameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var totalPriceLabel: UILabel!
+    
+}
