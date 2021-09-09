6dee7e08a697d078c28dc26d5f739c0c4b7510f9
shimyoungmin <dudals6965@gmail.com>
2021-09-09 18:30:03 +0900

    feat: [#15] Add setupImageViews function & scrollViewDidScroll function
    상세페이지 stack 뷰 안에 imageView추가하는 코드 작성 및 스크롤에 따라 헤더뷰 alpha변경하는 코드 추가

--- iOS/airbnb/airbnb/Presentation/MainSearchScene/AccommodationDetailViewController.swift
+++ iOS/airbnb/airbnb/Presentation/MainSearchScene/AccommodationDetailViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class AccommodationDetailViewController: UIViewController {
     
-    weak var coordinator: MainSearchSceneFlowCoordinator?
+    @IBOutlet weak var topHeaderView: UIView!
     @IBOutlet weak var accommodationImageStackView: UIStackView!
+    @IBOutlet weak var accommodationImageView: UIImageView!
     @IBOutlet weak var accommodationDetailView: AccommodationDetailView!
+    
+    weak var coordinator: MainSearchSceneFlowCoordinator?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAccommodationDetailViewController()
+        setupImageViews()
     }
     
     static func create() -> AccommodationDetailViewController {
@@ -32,7 +36,38 @@ class AccommodationDetailViewController: UIViewController {
         self.tabBarController?.tabBar.isHidden = true
     }
     
+    private func setupImageViews() {
+        self.accommodationImageView.image = UIImage(named: "background.png")
+        self.accommodationImageView.widthAnchor.constraint(equalTo: self.view.widthAnchor).isActive = true
+        self.accommodationImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.accommodationImageStackView.addArrangedSubview(accommodationImageView)
+
+        let view = UIImageView(frame: CGRect(origin: .zero, size: CGSize(width: self.accommodationImageView.frame.width, height: self.accommodationImageView.frame.height)))
+
+        view.image = UIImage(named: "thumbnail.png")
+        view.translatesAutoresizingMaskIntoConstraints = false
+        accommodationImageStackView.addArrangedSubview(view)
+    }
+    
     func injectionCoordinator(with coordinator: MainSearchSceneFlowCoordinator) {
         self.coordinator = coordinator
+    }
+}
+
+extension AccommodationDetailViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let topHeaderViewHeight: CGFloat = 112
+        let scrollViewOffset = accommodationDetailView.frame.minY - (topHeaderViewHeight*2) // 127
+        if scrollView.contentOffset.y > scrollViewOffset {
+            if scrollView.contentOffset.y > topHeaderViewHeight + scrollViewOffset {
+                self.topHeaderView.alpha = 1
+            } else {
+                self.topHeaderView.alpha = (scrollView.contentOffset.y - topHeaderViewHeight) / scrollViewOffset
+            }
+        } else {
+            self.topHeaderView.alpha = 0
+        }
     }
 }
