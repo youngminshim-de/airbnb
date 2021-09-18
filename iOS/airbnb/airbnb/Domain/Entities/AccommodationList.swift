fd01ce36bdf8197723725cea572c420287346207
shimyoungmin <dudals6965@gmail.com>
2021-09-18 16:07:00 +0900

    feat: [#21] Add AccommodationList Entity
    숙소리스트 페이지에 필요한 AccoomodationList Entity 구현

---
+++ iOS/airbnb/airbnb/Domain/Entities/AccommodationList.swift
@@ -0,0 +1,29 @@
+//
+//  AccommodationList.swift
+//  airbnb
+//
+//  Created by 심영민 on 2021/09/18.
+//
+
+import Foundation
+
+struct AccommodationList: Decodable {
+    var id: Int
+    var title: String
+    var images: [String]
+    var priceInfo: PriceInfo
+    var location: Location
+}
+
+struct PriceInfo: Decodable {
+    var pricePerNight: Int
+    var totalPrice: Int
+}
+
+struct Location: Decodable {
+    var longitude: Double
+    var latitude: Double
+    var country: String
+    var city: String
+    var district: String
+}
